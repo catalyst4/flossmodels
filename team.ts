8b30efead085100d9ef9962b6336da5ca9bf1068
catalystfour <jake@catalystfour.co.uk>
2021-02-16 16:39:29 +0000

    adam

--- team.ts
+++ team.ts
@@ -15,11 +15,11 @@ let team: Team[] = [
         img: 'jaden.jpg',
         bio: 'Meet Jaden, the youngest member of the team but don\'t let his youth fool you! He has a lot of experience doing social media and throughout the years has amassed a great amount of contacts also putting in a fair amount of work behind the scenes to ensure everything runs smoothly.'
     },
-    // {
-    //     name: 'Adam W',
-    //     img: 'adam.jpg',
-    //     bio: ''
-    // }
+    {
+        name: 'Adam W',
+        img: 'adam.jpg',
+        bio: 'Meet Adam, there isn\'t a section of Instagram he hasn\'t been involved in! Adam specialises in not only the OnlyFans scene, but U.K. & USA ran IG pages! He has a personal Instagram net of 2 million, split across both demographics! Jack of all trades?'
+    },
 ]
 
 export default team
